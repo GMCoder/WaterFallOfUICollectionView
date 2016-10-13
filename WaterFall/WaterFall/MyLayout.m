2c90fb0f66246405fcec280a204e2efe9520bf9b
GMCoder <GmCoder@163.com>
2016-10-13 22:48:21 +0800

    add

--- WaterFall/WaterFall/MyLayout.m
+++ WaterFall/WaterFall/MyLayout.m
@@ -32,30 +32,31 @@
         //创建一个布局属性类，通过indexPath来创建
         UICollectionViewLayoutAttributes * attris = [UICollectionViewLayoutAttributes layoutAttributesForCellWithIndexPath:index];
         //随机一个高度 在40——190之间
-        CGFloat hight = arc4random()%150+40;
+//        CGFloat hight = arc4random()%150+40;
         //哪一列高度小 则放到那一列下面
         //标记最短的列
-        int width=0;
-        if (colHight[0]<colHight[1]) {
-            //将新的item高度加入到短的一列
-            colHight[0] = colHight[0]+hight+self.minimumLineSpacing;
-            width=0;
-        }else{
-            colHight[1] = colHight[1]+hight+self.minimumLineSpacing;
-            width=1;
-        }
+//        int width=0;
+//        if (colHight[0]<colHight[1]) {
+//            //将新的item高度加入到短的一列
+//            colHight[0] = colHight[0]+hight+self.minimumLineSpacing;
+//            width=0;
+//        }else{
+//            colHight[1] = colHight[1]+hight+self.minimumLineSpacing;
+//            width=1;
+//        }
         
         //设置item的位置
-        attris.frame = CGRectMake(self.sectionInset.left+(self.minimumInteritemSpacing+WIDTH)*width, colHight[width]-hight-self.minimumLineSpacing, 100, 100);
+//        attris.frame = CGRectMake(self.sectionInset.left+(self.minimumInteritemSpacing+WIDTH)*width, colHight[width]-hight-self.minimumLineSpacing, 100, 100);
+        attris.frame = CGRectMake((i % 3) * 100 + self.minimumInteritemSpacing * (i % 3), self.sectionInset.top + (i / 3) * 100 + self.minimumLineSpacing * (i / 3), 100, 100);
         [_attributesArray addObject:attris];
     }
 
     //设置itemSize来确保滑动范围的正确 这里是通过将所有的item高度平均化，计算出来的(以最高的列位标准)
-    if (colHight[0]>colHight[1]) {
-        self.itemSize = CGSizeMake(WIDTH, (colHight[0]-self.sectionInset.top)*2/_itemCount-self.minimumLineSpacing);
-    }else{
-        self.itemSize = CGSizeMake(WIDTH, (colHight[1]-self.sectionInset.top)*2/_itemCount-self.minimumLineSpacing);
-    }
+//    if (colHight[0]>colHight[1]) {
+//        self.itemSize = CGSizeMake(WIDTH, (colHight[0]-self.sectionInset.top)*2/_itemCount-self.minimumLineSpacing);
+//    }else{
+//        self.itemSize = CGSizeMake(WIDTH, (colHight[1]-self.sectionInset.top)*2/_itemCount-self.minimumLineSpacing);
+//    }
     
 }
 
@@ -66,9 +67,9 @@
 
 
 
-//- (CGSize)collectionViewContentSize
-//{
-//    return CGSizeMake(400, 1000);
-//}
+- (CGSize)collectionViewContentSize
+{
+    return CGSizeMake(self.collectionView.frame.size.width, (_itemCount / 3) * (100 + self.minimumInteritemSpacing));
+}
 
 @end
